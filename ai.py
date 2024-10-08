7347a97303cd099dac7c1318040122d841700c20
you-mo77 <kano1201971@gmail.com>
2024-10-08 21:50:21 +0900

    クラス化

--- ai.py
+++ ai.py
@@ -2,37 +2,52 @@ import librosa as lib
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
-# 音声ファイル読み込んでメルスペクトログラム化[単一の音声ファイルパス -> dbスケールメルスペクトログラム, サンプリング周波数]
-def create_spectrogram(path:str):
-    # 音声ファイル読み込み
-    y, sr = lib.load(path, sr=None)
+# 各音声ファイルのクラス
+class AudioFile:
+    # イニシャライザ [アーティスト名、曲名、パス] 曲名に関してはパスから引き出せるかも
+    def __init__(self, artist_name, track_name, file_path):
+        self.artist_name = artist_name # アーティスト名
+        self.track_name = track_name   # 曲名
+        self.file_path = file_path     # wavファイルパス
+        self.sr = None                 # サンプリング周波数
+        self.spectrogram = None        # dbスケールのメルスペクトログラム
 
-    # メルスペクトログラム作成
-    spectrogram = lib.feature.melspectrogram(y=y, sr=sr)
+        # 音声ファイル読み込みとスペクトログラム[sr, spectrogram設定]
+        self.create_spectrogram()
 
-    # dbスケール変換
-    spec_db = lib.power_to_db(spectrogram, ref=np.max)
+    # 音声ファイル読み込んでメルスペクトログラム化[単一の音声ファイルパス -> dbスケールメルスペクトログラム, サンプリング周波数]
+    def create_spectrogram(self):
+        # 音声ファイル読み込み
+        y, sr = lib.load(self.file_path, sr=None)
 
-    return spec_db, sr
+        # サンプリング周波数設定
+        self.sr = sr
 
-# スペクトログラム表示[スペクトログラム(np配列), サンプリング周波数]
-def show_spectrogram(spec_db:np.ndarray, sr:int):
-    # グラフ作成
-    plt.figure()
+        # メルスペクトログラム作成
+        spectrogram = lib.feature.melspectrogram(y=y, sr=sr)
 
-    # スペクトログラム描画
-    lib.display.specshow(spec_db, sr=sr, x_axis="time", y_axis="mel")
+        # dbスケール変換
+        spec_db = lib.power_to_db(spectrogram, ref=np.max)
 
-    # 詳細追加
-    plt.rcParams["font.family"] = "Meiryo"
-    plt.title("Mel-frequency spectrogram")
-    plt.tight_layout()
+        # スペクトログラム設定
+        self.spectrogram = spec_db
 
-    # 表示
-    plt.show()
+    # スペクトログラム表示[スペクトログラム(np配列), サンプリング周波数]
+    def show_spectrogram(self):
+        # グラフ作成
+        plt.figure()
 
-    return 
+        # スペクトログラム描画
+        lib.display.specshow(self.spectrogram, sr=self.sr, x_axis="time", y_axis="mel")
+
+        # 詳細追加
+        plt.title("Mel-frequency spectrogram")
+        plt.tight_layout()
+
+        # 表示
+        plt.show()
 
 # 変数の詳細を出力(テスト用)[何らかの変数]
 def test_output(output_variable):
@@ -44,9 +59,9 @@ def test_output(output_variable):
 
 # テスト関数(好きにいじる)
 def test():
-    spec_db, sr = create_spectrogram(r"C:\Users\重岡拓郎\Desktop\code\voice\sound_file\iyowa\1000年生きてる.wav")
-    #test_output(spec_db)
-    show_spectrogram(spec_db, sr)
+    test_audio = AudioFile("iyowa", "1000年生きてる", r"sound_file\iyowa\1000年生きてる.wav")
+    test_audio.show_spectrogram()
+    
 
 # メイン関数
 def main():
--- test.py
+++ test.py
@@ -2,14 +2,18 @@ import numpy as np
 import librosa as lib
 
 def main():
+    """
     path = r"sound_file\ヨルシカ - 藍二乗 (Music Video) [4MoRLTAJY_0].wav"
     data, fs = lib.load(path)
 
     # ピッチ
     # ピッチ=基本周波数(F0) stftの結果各フレームごとにピッチを算出 -> numpy配列に代入(各列がフレーム、各行が周波数に対応)
     pitches, magnitudes = lib.core.piptrack(y=data, sr=fs)
-    for t in range(pitches.shape[1]):
-        
+    """
+    data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    temp = np.array([s.flatten() for s in data])
+    print(temp)
+
         
 
 
